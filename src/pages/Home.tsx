81b38b83d3fc0ab17d4e1fcdc478bd0a02174fe2
C4ncino <193305@iberopuebla.mx>
2024-03-16 10:18:02 -0600

    Init

---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import PageTemplate from "@assets/PageTemplate";
+
+const Home = () => {
+    return (
+        <PageTemplate>
+            <h1>Hola</h1>
+        </PageTemplate>
+    );
+}
+
+export default Home;
